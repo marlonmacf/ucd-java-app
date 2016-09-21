f021fc8022f4f67ad3ffa670410b20eceb3b4aed
Iago <iago.olvr@gmail.com>
2016-09-20 21:47:24 -0300

    star to do the photoFragment

--- app/src/main/java/ucd/app/views/fragments/PhotoFragment.java
+++ app/src/main/java/ucd/app/views/fragments/PhotoFragment.java
@@ -1,10 +1,13 @@
 package ucd.app.views.fragments;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import ucd.app.R;
 
@@ -23,6 +26,8 @@ public class PhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_photo, container, false);
+
+
     }
 
 }
