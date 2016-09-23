fe5e0206e0c7396478929dfa9f9dc76e3a29fb05
Marlon Andrel <marlonmacf@gmail.com>
2016-09-22 22:05:46 -0300

    Updated ranking layout and applied the retrofit API service to get the user rank list.

--- app/src/main/java/ucd/app/views/fragments/RankingFragment.java
+++ app/src/main/java/ucd/app/views/fragments/RankingFragment.java
@@ -10,7 +10,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -26,15 +28,14 @@ public class RankingFragment extends Fragment {
     // Service for access the RETROFIT API.
     private ApiService apiService;
 
-    //  ProgressBar for loading the requests.
-    //private ProgressBar progressBar;
+    private ProgressBar progressBar;
 
-    private ListView listView ;
+    private ListView listView;
 
-    private String[] values;
+    private List<String> values;
 
     public RankingFragment() {
-
+        // Required empty public constructor
     }
 
     @Override
@@ -44,73 +45,46 @@ public class RankingFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_ranking, container, false);
-
-        // Get ListView object from xml
-        this.listView = (ListView)view.findViewById(R.id.ranking_list);
-
-        // Defined Array values to show in ListView
-        values = new String[] { "1 |    Android List View",
-                "2 |    Adapter implementation",
-                "3 |    Simple List View In Android",
-                "4 |    Create List View Android",
-                "5 |    Android Example",
-                "6 |    List View Source Code",
-                "7 |    List View Array Adapter",
-                "8 |    Android Example List View"
-        };
-
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        // Assign adapter to ListView
-        listView.setAdapter(adapter);
+        final View view = inflater.inflate(R.layout.fragment_ranking, container, false);
 
         // Booting the service API and the progressBar.
         this.apiService = ApiClient.getClient().create(ApiService.class);
-        //this.progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        this.progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        this.listView = (ListView) view.findViewById(R.id.ranking_list);
+        this.values = new ArrayList<>();
 
         // Start loading
-        //progressBar.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
 
         // Fetching for all users.
-//        apiService.fetchUsers().enqueue(new Callback<List<User>>() {
+        apiService.fetchUsers().enqueue(new Callback<List<User>>() {
 
-        //@Override
-//        public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-//
-//        // Make a objecto of users and show on the view.
-////        TextView text_view_users = (TextView) findViewById(R.id.text_view_users);
-////        text_view_users.setText("USERS: \n" + response.body().toString());
-//
-//            values[0] = response.body().toString();
-//
-//        // Finish the loading.
-//        //progressBar.setVisibility(View.INVISIBLE);
-//        }
-//
-//        //@Override
-//        public void onFailure(Call<List<User>> call, Throwable t) {
-//
-//        // Throw friendly exception.
-//        //Toast.makeText(MainActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
-//
-//        // Finish the loading.
-//        //progressBar.setVisibility(View.INVISIBLE);
-//        }
-//
-//        });
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
 
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
+                // Make a objecto of users and show on the view.
+                int count = 1;
+                for (User user : response.body()) {
+                    values.add((count++) + "º   " + user.getName() + " " + user.getScore() + "pts");
+                }
+
+                // Assign adapter to ListView
+                listView.setAdapter(new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, values));
+
+                // Finish the loading.
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable throwable) {
+                Toast.makeText(view.getContext(), throwable.toString(), Toast.LENGTH_SHORT).show();
+
+                // Finish the loading.
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+        });
 
         return view;
     }
-
-
-
 }
