133aa1fbea14288adc9ed49b7c294983ffc9009e
Marlon Andrel <marlonmacf@gmail.com>
2016-10-10 22:13:17 -0300

    Google maps yooooooo!!!

--- app/src/main/java/ucd/app/views/activities/MainActivity.java
+++ app/src/main/java/ucd/app/views/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package ucd.app.views.activities;
 
-
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.design.widget.TabLayout;
@@ -19,6 +18,12 @@ import ucd.app.views.fragments.TaskFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final android.support.v4.app.Fragment infoFragment = new InfoFragment();
+    private final android.support.v4.app.Fragment photoFragment = new PhotoFragment();
+    private final android.support.v4.app.Fragment placeFragment = new PlaceFragment();
+    private final android.support.v4.app.Fragment taskFragment = new TaskFragment();
+    private final android.support.v4.app.Fragment rankingFragment = new RankingFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +37,18 @@ public class MainActivity extends AppCompatActivity {
         // Defines the number of tabs by setting appropriate fragment and tab name.
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new InfoFragment());
-        adapter.addFragment(new PhotoFragment());
-        adapter.addFragment(new PlaceFragment());
-        adapter.addFragment(new TaskFragment());
-        adapter.addFragment(new RankingFragment());
+        adapter.addFragment(this.infoFragment);
+        adapter.addFragment(this.photoFragment);
+        adapter.addFragment(this.placeFragment);
+        adapter.addFragment(this.taskFragment);
+        adapter.addFragment(this.rankingFragment);
         viewPager.setAdapter(adapter);
+
+        // Pushing MapView Fragment
+//        Fragment fragment = Fragment.instantiate(this.placeFragment.getContext(), PlaceFragment.class.getName());
+//        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.container, fragment);
+//        fragmentTransaction.commit();
 
         // Assigns the ViewPager to TabLayout, Tabs with only Icons.
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
@@ -47,11 +58,5 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_tab_place_dark);
         tabLayout.getTabAt(3).setIcon(R.drawable.ic_tab_task_dark);
         tabLayout.getTabAt(4).setIcon(R.drawable.ic_tab_ranking_dark);
-
-        // Pushing MapView Fragment
-//        Fragment fragment = Fragment.instantiate(this, PlaceFragment.class.getName());
-//        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-//        fragmentTransaction.replace(R.id.container, fragment);
-//        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/ucd/app/views/fragments/PlaceFragment.java
+++ app/src/main/java/ucd/app/views/fragments/PlaceFragment.java
@@ -6,13 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import ucd.app.R;
 
 public class PlaceFragment extends Fragment {
 
-    private SupportMapFragment map;
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     public PlaceFragment() {
         // Required empty public constructor
@@ -27,8 +35,61 @@ public class PlaceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_place, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_place, container, false);
 
-        return view;
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+//        try {
+//            MapsInitializer.initialize(getActivity().getApplicationContext());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+//        mMapView.getMapAsync(new OnMapReadyCallback() {
+//            @Override
+//            public void onMapReady(GoogleMap mMap) {
+//                googleMap = mMap;
+//
+//                // For showing a move to my location button
+//                googleMap.setMyLocationEnabled(true);
+//
+//                // For dropping a marker at a point on the Map
+//                LatLng sydney = new LatLng(-34, 151);
+//                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+//
+//                // For zooming automatically to the location of the marker
+//                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+//                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//            }
+//        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
